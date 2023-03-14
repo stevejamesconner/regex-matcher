6122e9bc62cf18c23ee88d78d02c6f86d339fad6
Steve Conner <steveconner@gmail.com>
2023-03-14 09:03:12 +0000

    Testing v1

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,31 +1,8 @@
 import {matcher} from '../src/matcher'
-import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
 import {expect, test} from '@jest/globals'
-
-
-
-// test('throws invalid number', async () => {
-//   const input = parseInt('foo', 10)
-//   await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-// })
-//
-// test('wait 500 ms', async () => {
-//   const start = new Date()
-//   await wait(500)
-//   const end = new Date()
-//   var delta = Math.abs(end.getTime() - start.getTime())
-//   expect(delta).toBeGreaterThan(450)
-// })
-//
-// // shows how the runner will run a javascript action with env / stdout protocol
-// test('test runs', () => {
-//   process.env['INPUT_MILLISECONDS'] = '500'
-//   const np = process.execPath
-//   const ip = path.join(__dirname, '..', 'lib', 'main.js')
-//   const options: cp.ExecFileSyncOptions = {
-//     env: process.env
-//   }
-//   console.log(cp.execFileSync(np, [ip], options).toString())
-// })
+test('Extracts Number From String', async () => {
+  const matches = await matcher('\d+', 'v1/develop', 'g')
+  const matchesArray = Array.from(matches)
+  expect(matchesArray.length).toBe(1)
+  expect(matchesArray[0]).toBe('1')
+})
