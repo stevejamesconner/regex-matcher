fec42783e56fe3f5a1558c259ac746e743616117
Steve Conner <steveconner@gmail.com>
2023-03-14 09:13:20 +0000

    Testing v1

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,8 +1,9 @@
 import {matcher} from '../src/matcher'
 import {expect, test} from '@jest/globals'
 test('Extracts Number From String', async () => {
-  const matches = await matcher('\d+', 'v1/develop', 'g')
+  const matches = await matcher('\\d+', 'v1/develop', 'g')
   const matchesArray = Array.from(matches)
   expect(matchesArray.length).toBe(1)
-  expect(matchesArray[0]).toBe('1')
+  expect(matchesArray[0].length).toBe(1)
+  expect(matchesArray[0][0]).toBe('1')
 })
